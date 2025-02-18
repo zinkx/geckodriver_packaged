e84fcf099767e6c235ddbbd15c92f47464aacfff
zinkx <valeriaalexandra.schmidt@gmail.com>
2025-02-18 19:26:52 +0100

    updated dist and readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-This is a packaged version of geckodriver for offline use.
+# Geckodriver Wrapper
+
+This package provides a wrapped version of `geckodriver.exe` for use in offline environments.
+
+## Installation
+```bash
+pip install geckodriver-wrapper
+```
+
+## Usage
+```python
+from geckodriver_wrapper import get_geckodriver_path
+from selenium import webdriver
+
+driver = webdriver.Firefox(executable_path=get_geckodriver_path())
+driver.get("https://example.com")
+```
+
+## Driver Source
 Driver Release: https://github.com/mozilla/geckodriver/releases/tag/v0.35.0 geckodriver-v0.35.0-win32.zip
+
+## Driver Licence
 Driver License: https://github.com/mozilla/geckodriver?tab=MPL-2.0-1-ov-file#readme MPL-2.0 license
+
+
+
--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="geckodriver-wrapper",
-    version="0.2",
-    packages=find_packages(),  # Ensures Python finds the package
-    include_package_data=True,  # Includes non-Python files like .exe
-    package_data={"geckodriver_wrapper": ["geckodriver.exe"]},  # Explicitly include binary
+    version="0.4",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"geckodriver_wrapper": ["geckodriver.exe"]},
     description="A packaged version of geckodriver for offline use.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zinkx/geckodriver_packaged",  # GitHub repo URL
+    project_urls={
+        "Documentation": "https://github.com/zinkx/geckodriver_packaged#readme",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
